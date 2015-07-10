f99665a5cda6b36983dba13db6131c0994b0a7a8
Aaron Kuehler <aaron.kuehler@gmail.com>
2015-07-10 19:51:22 -0400

    rename pipe reader and writers
    Clarify the names of the variables which represent the pipe ends

--- lib/futurist/future.rb
+++ lib/futurist/future.rb
@@ -9,14 +9,14 @@ module Futurist
       @pipe = IO.pipe
       @worker_pid = fork_worker_process
     end
-    
+
     def value
       if @value == :no_value
         @value = read_worker_results
       end
       @value
     end
- 
+
     private
     attr_accessor :worker_pid
     attr_reader :arguments,
@@ -25,26 +25,26 @@ module Futurist
                 :worker_pid
 
     def fork_worker_process
-      read, write = pipe
+      reader, writer = pipe
 
       fork do
-        read.close
+        reader.close
         begin
           result = block.call(arguments)
         rescue => e
           result = e
         end
-        Marshal.dump(result, write)
+        Marshal.dump(result, writer)
         exit!(0)
       end
     end
 
     def read_worker_results
-      read, write = pipe
+      reader, writer = pipe
 
-      write.close
+      writer.close
       Process.wait(worker_pid)
-      raw_result = read.read
+      raw_result = reader.read
       result = Marshal.load(raw_result)
       if result.kind_of?(Exception)
         raise result
