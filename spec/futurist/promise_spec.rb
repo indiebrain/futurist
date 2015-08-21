c4f4c33d0303d3e15ac64fe4b64947a88c74c610
Aaron Kuehler <aaron.kuehler@gmail.com>
2015-08-21 13:04:06 -0400

    move subprocess error handling
    When an error occurs while executing the promise in a subprocess, the
    error needs to be captured and serialized across the pipe back to the
    parent process. The promise itsef doesn't necessarily care that this
    special handling needs to occur; the special handling is necessary
    because the Promise is evaluated in a child process. As such, it seems
    more approprate for the process managing object to coordinate the
    special marshalling of the error state across processes.

--- spec/futurist/promise_spec.rb
+++ spec/futurist/promise_spec.rb
@@ -9,16 +9,4 @@ describe Futurist::Promise do
     expect(promise.value).
       to eq(value)
   end
-
-  it "has a value of an error instance when the callable raises an error" do
-    error = StandardError.new("expected")
-    callable = Proc.new { fail error }
-    promise = Futurist::Promise.new(callable: callable)
-
-    expect { promise.value }.
-      to_not raise_error
-
-    expect(promise.value).
-      to eq(error)
-  end
 end
