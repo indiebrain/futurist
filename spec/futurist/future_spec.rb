b8dcbf4ad3a0a234e3556ae8ebdc6522e7969c18
Aaron Kuehler <aaron.kuehler@gmail.com>
2016-02-08 09:54:30 -0500

    Extract integration specs
    Separate integration specs of the Future object from its unit specs.

--- spec/futurist/future_spec.rb
+++ spec/futurist/future_spec.rb
@@ -1,21 +1,6 @@
 require "spec_helper"
 
 describe Futurist::Future do
-  it "receives the value of its promise at some future point in time" do
-    value = "value"
-    future = Futurist::Future.new { value }
-
-    expect(future.value).
-      to eq(value)
-  end
-
-  it "reraises exceptions which occur in its promise's execution" do
-    class FakeError < StandardError; end
-    future = Futurist::Future.new { fail(FakeError, "fail") }
-
-    expect { future.value }.
-      to raise_error(FakeError, "fail")
-  end
 
   it "is ready after the promise's value has finished calculating" do
     class ResolvedResolutionStrategy < FakeResolutionStrategy
