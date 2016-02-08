0b48992f16fbd6ce368b0593e835ed18642af515
Aaron Kuehler <aaron.kuehler@gmail.com>
2016-02-08 09:55:26 -0500

    Reduce coupling of Future and its ResolutionStrategy
    Instead of passing a concrete implementation of a resolution strategy
    into the Future object, pass a callable which can instantiate the
    resolution strategy. This allows a more flexible approach to telling
    the Future how it should resolve its value which makes testing the
    Future in isolation a bit easier.

--- spec/futurist/future_spec.rb
+++ spec/futurist/future_spec.rb
@@ -3,50 +3,40 @@ require "spec_helper"
 describe Futurist::Future do
 
   it "is ready after the promise's value has finished calculating" do
-    class ResolvedResolutionStrategy < FakeResolutionStrategy
-      def resolved?
-        true
-      end
-    end
+    resolution_strategy = double("ResolutionStrategy",
+                                 resolved?: true)
+    resolution_strategy_constructor = ->(_) { resolution_strategy }
 
-    value = "value"
     future = Futurist::Future.new(
-      resolution_strategy: ResolvedResolutionStrategy
-    ) { value }
-
-    future.value
+      resolution_strategy_constructor: resolution_strategy_constructor
+    ) { "value" }
 
     expect(future).
       to be_ready
   end
 
   it "is not ready before the promise's value has finished calculating" do
-    class NotResolvedResolutionStrategy < FakeResolutionStrategy
-      def resolved?
-        false
-      end
-    end
-
-    value = "value"
+    resolution_strategy = double("ResolutionStrategy",
+                                 resolved?: false)
     future = Futurist::Future.new(
-      resolution_strategy: NotResolvedResolutionStrategy
+      resolution_strategy_constructor: ->(_) { resolution_strategy }
     ) { value }
     allow(future).
       to receive(:start_promise_evaluation)
-
-    future.value
 
     expect(future).
       to_not be_ready
   end
 
-  class FakeResolutionStrategy
-    def initialize(promise:)
-      @promise = promise
-    end
+  it "is valued by the resolution of its promise" do
+    resolution_strategy = double("ResolutionStrategy",
+                                 resolve: "value")
 
-    def resolve
-      @promise.value
-    end
+    future = Futurist::Future.new(
+      resolution_strategy_constructor: ->(_) { resolution_strategy }
+    )
+
+    expect(future.value).
+      to eq("value")
   end
 end
