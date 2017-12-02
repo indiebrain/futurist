27a125938f24f2e759a134549a8fa49ccfd47280
Aaron Kuehler <aaron.kuehler@gmail.com>
2017-12-02 14:24:58 -0500

    @wip - Add bounding to the bounder

--- lib/futurist/bounder.rb
+++ lib/futurist/bounder.rb
@@ -5,10 +5,13 @@ module Futurist
     extend Forwardable
 
     def initialize(concurrencey:, strategy:)
+      @backlog = Queue.new
       @concurrencey = concurrencey
-      @run_list = []
-      @run_list_lock = Mutex.new
+      @running = []
+      @running_lock = Mutex.new
       @strategy = strategy
+
+      run
     end
 
     def call(promise:)
@@ -20,13 +23,37 @@ module Futurist
 
     private
 
-    attr_reader :concurrencey, :run_list, :run_list_lock, :strategy
+    attr_reader :backlog, :concurrencey, :running, :running_lock, :strategy
 
-    def_delegator :run_list_lock, :synchronize
+    def_delegator :running_lock, :synchronize
 
     def add_to_run_list(resolvable)
-      resolvable.run
-      run_list << resolvable
+      backlog.push(resolvable)
+    end
+
+    def run
+      Thread.new do
+        loop do
+          clear_resolved
+          schedule_new_work
+        end
+      end
+    end
+
+    def clear_resolved
+      synchronize do
+        running.delete_if(&:resolved?)
+      end
+    end
+
+    def schedule_new_work
+     synchronize do
+       (concurrencey - running.size).times do
+         resolvable = backlog.pop
+         resolvable.run
+         running << resolvable
+       end
+     end
     end
   end
 end
